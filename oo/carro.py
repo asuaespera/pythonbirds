469d6417736378e575b292bb7bb4fd9eb2f504f2
Carlos <asuaespera.x@gmail.com>
2021-11-20 00:08:51 -0300

    Implementada a direção

--- oo/carro.py
+++ oo/carro.py
@@ -42,16 +42,16 @@ O   L
     >>> direcao = Direcao()
     >>> direcao.valor
     'Norte'
-    >>> direcao.girar_a_direta()
+    >>> direcao.girar_a_direita()
     >>> direcao.valor
     'Leste'
-    >>> direcao.girar_a_direta()
+    >>> direcao.girar_a_direita()
     >>> direcao.valor
     'Sul'
-    >>> direcao.girar_a_direta()
+    >>> direcao.girar_a_direita()
     >>> direcao.valor
     'Oeste'
-    >>> direcao.girar_a_direta()
+    >>> direcao.girar_a_direita()
     >>> direcao.valor
     'Norte'
     >>> direcao.girar_a_esquerda()
@@ -91,6 +91,29 @@ O   L
     >>> 'Oeste'
 """
 
+NORTE = "Norte"
+SUL = "Sul"
+LESTE = "Leste"
+OESTE = "Oeste"
+
+
+class Direcao:
+    rotacao_a_direita_dct = {
+        NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE
+    }
+    rotacao_a_esquerda_dct = {
+        NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL
+    }
+
+    def __init__(self):
+        self.valor = NORTE
+
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
+
 
 class Motor:
     def __init__(self):
@@ -102,9 +125,3 @@ class Motor:
     def frear(self):
         self.velocidade -= 2
         self.velocidade = max(0, self.velocidade)
-
-
-
-
-
-
